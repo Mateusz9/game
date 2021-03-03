8f33f92893dc6f90a09a8eb562439c7c491501d7
Mateusz Kazimierczak <M.Kazimierczak@EscuelaEuropea.org>
2021-03-03 14:28:07 +0100

    init commit

---
+++ game.py
@@ -0,0 +1,46 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      m.kazimierczak
+#
+# Created:     03/03/2021
+# Copyright:   (c) m.kazimierczak 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import sys, pygame
+pygame.init()
+
+size = width, height = 1200, 800
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
+
+bar = pygame.image.load("bar.png")
+barrect = bar.get_rect()
+
+barrect = barrect.move((200, 700))
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    if ballrect.colliderect(barrect):
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    screen.blit(bar, barrect)
+    pygame.display.flip()
+    pygame.time.delay(100)
